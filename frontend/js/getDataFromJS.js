d0e3ed1a34ff61549af672857b6cb9a95fba78b4
noemiko <noemikow8@gmail.com>
2016-05-17 18:29:45 +0200

    animacje

--- frontend/js/getDataFromJS.js
+++ frontend/js/getDataFromJS.js
@@ -4,45 +4,44 @@ $(document).ready(function () {
 });
 function getJS(){
 	 $.ajax({
-                    url: "/jakniedojade/api/connections/",
+                    url: "zadanie.json",
                     dataType: "text",
                     success: function(data) {
 						createTasksFromJs(data);
 					}							
-					});					
-	            
+					});					     
 };
 function createTasksFromJs(data){
 	  var json = $.parseJSON(data);
 						 $.each( json, function( index, value ){
 							square="<div class='col-lg-3 col-md-4 col-xs-6 thumb'>\
-										<a class='thumbnail' data-name='"+value.name+"' data-description='"+value.description+"' href='#'>\
+										<div style='border=0px' class='thumbnail' data-name='"+value.name+"' data-description='"+value.description+"' href='#'> \
 											<img class='img-responsive' src="+value.image_url+" alt=''>\
-											<div  class='underSquare'>\
-												<img class='img-responsive hand' src='connects/lapka.png' alt=''>\
-												<span class='voit'>0</span>\
+											<div class='VoteBar' >\
+												<img  class=' hand' style='vertical-align:middle float: left;' src='connects/lapka.png'>\
+												<span class='vote'>0</span>\
 											</div>\
-										</a>\
-									</div>"
+										</div>\
+									</div>";
 								$('.row').append(square);
 									modal();						
 							});	
-	
-}
+};
+
 
 function modal(){
-	 $(".thumbnail").click(function(){
+	 $(".img-responsive").click(function(){
         $("#myModal").modal();
-        $(".modal-title").html($(this).data('name'));
-        $(".modal-body").html($(this).data('description'));
+        $(".modal-title").html($(this).parent().data('name'));
+        $(".modal-body").html($(this).parent().data('description'));
 		initMap();
 		
     });
-}
+};
 function initMap() {
         var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv, {
           center: {lat: 44.540, lng: -78.546},
           zoom: 8
         });
-      }
+      };
