7922aa3bbfebcd02335c05a8f966ea10612a1ffe
Firemark <marpiechula@gmail.com>
2016-05-24 20:44:07 +0200

    i hate you django rest framework

--- jakniedojade/jakniedojade/settings.py
+++ jakniedojade/jakniedojade/settings.py
@@ -23,10 +23,18 @@ class Base(Configuration):
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
+
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.BasicAuthentication',
+        ),
+    }
 
     ROOT_URLCONF = 'jakniedojade.urls'
 
