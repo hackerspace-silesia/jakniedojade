c1a3a28f208ea6222f8232efe3b16c11188b7414
Firemark <marpiechula@gmail.com>
2016-05-24 19:49:05 +0200

    fix image

--- jakniedojade/app/serializers.py
+++ jakniedojade/app/serializers.py
@@ -1,12 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework.fields import CharField, IntegerField
+from django.conf import settings
+
 from app.models import Connection
 
 class ConnectionSerializer(ModelSerializer):
     image_url = SerializerMethodField()
     vote_count = IntegerField()
     
-    SerializerMethodField()
+    def get_image_url(self, obj):
+        return '{}/{}'.format(settings.MEDIA_URL, obj.image_url)
 
     class Meta:
         model = Connection
