748a65a775b6f928c541485ff5ef098b6d7f130e
noemiko <noemikow8@gmail.com>
2016-05-24 21:30:16 +0200

    vote

--- jakniedojade/app/static/js/main.js
+++ jakniedojade/app/static/js/main.js
@@ -15,7 +15,7 @@ function createTasksFromJs(data){
 	  var json = $.parseJSON(data);
 						 $.each( json, function( index, value){
 							var square="<div class='col-lg-3 col-md-4 col-xs-6 thumb'>\
-										<div style='border=0px' class='thumbnail' data-id='"+value.id+"' data-map='"+value.Iframe_url+"' data-name='"+value.name+"' data-description='"+value.description+"' href='#'> \
+										<div style='border=0px' class='thumbnail' data-id='"+value.id+"' data-map='"+value.iframe_url+"' data-name='"+value.name+"' 												data-description='"+value.description+"' href='#'> \
 											<img class='img-responsive' width='350' src="+value.image_url+" alt=''>\
 											<div class='VoteBar' >\
 												<img  class=' hand' style='vertical-align:middle float: left;' src='/static/img/lapka.png'>\
@@ -58,16 +58,24 @@ function sendVote(id){
 	  $.ajax({
             type:"POST",
             url:"/api/connections/"+id+"/vote/", 
-                success:function(data) {
-                $('.modal-footer').html('');
-								$('.modal-title').html('Dziękujemy za zagłosowanie');
-								addVoteToHand(id);
-                },		
-        });						
+                success:function(data) {				
+                			$('.modal-footer').html('');
+							$('.modal-title').html('Dziękujemy za zagłosowanie');
+							addVoteToHand(id);
+                },	
+				error:function( xhr,textStatus,err) {
+					if (xhr.status==409)
+						{
+							$('.modal-title').html('Niestety nie możesz już głosować na tą trasę');	
+							$('.modal-footer').html('');
+						}
+								
+        }
+});
+};						
 					     
-};
 function addVoteToHand(id){
-	var numberOfVote= $(id);
+	var numberOfVote= $('#'+id);
   							var num = parseInt(numberOfVote.text());
   							numberOfVote.text(num+1);
 };
