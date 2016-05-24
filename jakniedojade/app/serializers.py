3a5e83133b1ce8d8311f1047ff0c28ef84584f99
Firemark <marpiechula@gmail.com>
2016-05-24 19:47:01 +0200

    restore files

--- jakniedojade/app/serializers.py
+++ jakniedojade/app/serializers.py
@@ -3,8 +3,11 @@ from rest_framework.fields import CharField, IntegerField
 from app.models import Connection
 
 class ConnectionSerializer(ModelSerializer):
-    image_url = CharField()
+    image_url = SerializerMethodField()
     vote_count = IntegerField()
+    
+    SerializerMethodField()
+
     class Meta:
         model = Connection
         fields = (
