22594c07a7d33263feac92ac7416fd301f2d141c
noemiko <noemikow8@gmail.com>
2016-06-27 11:30:00 +0200

    repait $http

--- public/js/Tracks.js
+++ public/js/Tracks.js
@@ -1,35 +1,36 @@
 angular.module('Tracks')
   .controller('TracksController', ['$modal','$scope', '$http',function ( $modal,$scope, $http) 
-{		
-	function getInformationAboutTracks()
-		{
-			$scope.loading = true;
+{		var track = this;
+	
+			track.loading = true;
   		 $http({
 				  method: 'GET',
 				  url: '/api/connections/'
 				})
   		 .success(function(data, status, headers, config) 
 			    {
-			     $scope.tracks=data;
+			     track.dataTrack = data;
 				})
   		 .error(function(data, status, headers, config)
   		 		 {
 			      console.log(headers);
 				});
-  		} ;
-
-	$scope.sendVote = function()
+  		
+  	track.getActualTrack=function(){
+  		return actualTrack;
+  	}
+	track.sendVote = function()
 		{
 		      console.log('send'); 
       	};
 //modals------------------
-    $scope.showModalInformation=function(trackObject)
+    track.showModalInformation=function(trackObject)
     {
+    	
     	var dialog = $modal.open
     	({
-			templateUrl: 'information',
+			templateUrl: 'modalWithInformation.html',
 			controller: ModalInfoController,
-			scope:$scope.$new(true),
 			resolve: 
 			{
 				actualTrack: function () 
@@ -41,17 +42,10 @@ angular.module('Tracks')
 		})
 	};
 
-	var init = function () 
-	{
-          getInformationAboutTracks();
-      };
-
-      init();
-
    var ModalInfoController = function 
-   ($scope, $modalInstance, actualTrack, $sce) 
+   ($scope, $modalInstance, Track, $sce) 
    		{
-          $scope.actualTrack = actualTrack;
+         $scope.actualTrack = Track;
           $scope.ok = function () 
           {
               $modalInstance.close();
@@ -59,14 +53,9 @@ angular.module('Tracks')
 
       };
 
-      ModalInfoController['$inject'] = 
-      ['$scope', '$modalInstance', 'actualTrack','$sce'];
+      ModalInfoController['$inject'] = ['$scope', '$modalInstance', 'actualTrack','$sce'];
 
-      var init = function () {
-      };
-      init();
-
-      	 $scope.showModalAccept=function(trackName,trackId)
+     track.showModalAccept=function(trackName,trackId)
     {
     	var dialog = $modal.open
     	({
@@ -75,10 +64,6 @@ angular.module('Tracks')
 			scope:$scope.$new(true),
 			resolve: 
 			{
-				trackNameToVote: function () 
-				{
-				return trackName;
-				},
 				trackId: function () 
 				{
 				return trackId;
@@ -87,13 +72,13 @@ angular.module('Tracks')
 			size: 'lg'
 		})
 	};
-	   var ModalAccept = function ($scope, $modalInstance,trackNameToVote,trackId, $sce) 
+	   var ModalAccept = function ($scope, $modalInstance,trackId, $sce) 
    		{
-          $scope.title ="Czy na pewno chcesz głosować na '"+trackNameToVote+"'";
+          $scope.title ="Czy na pewno chcesz zagłosować? ";
           $scope.showButtons=true;
           $scope.trackId = trackId;
           $scope.ok = function () 
-          {		$scope.title ='zalezy od zwrotu'
+          {	$scope.title ='zalezy od zwrotu'
               $scope.showButtons=false;
 
           };
@@ -104,9 +89,5 @@ angular.module('Tracks')
       };
 
       ModalAccept['$inject'] = ['$scope', '$modalInstance', 'trackNameToVote','trackId','$sce'];
-
-      var init = function () {
-      };
-      init();
 
 }]);
